fa04c1290fe6ae59f56fefce38a157733321b8ef
harutiro <harutiro2027@outlook.jp>
2021-01-10 14:21:05 +0900

    fix: どこでも参照できるようにグローバル変数に変更した

--- app/src/main/java/app/makino/harutiro/resito2/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/resito2/MainActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
+
+    var nedanDateView:List<OkaneListDateResycle>? = null
+    var adapter: RecyclerViewAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recycleView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.resycleView)
 
-        var nedanDateView: List <OkaneListDateResycle> = listOf(
+        nedanDateView = listOf(
             OkaneListDateResycle("20xx-aa-bb",1,R.drawable.image1,R.drawable.image2,1),
             OkaneListDateResycle("20xx-aa-bb",1,R.drawable.image1,R.drawable.image2,1),
             OkaneListDateResycle("20xx-aa-bb",1,R.drawable.image1,R.drawable.image2,1)
@@ -19,12 +23,12 @@ class MainActivity : AppCompatActivity() {
         )
 
 
-        val adapter = RecyclerViewAdapter(this)
+        adapter = RecyclerViewAdapter(this)
         recycleView.layoutManager = LinearLayoutManager(this)
         recycleView.adapter = adapter
 
         //リサイクラービューアダプターで宣言したaddAllメソッドを呼んであげてデータも渡している
-        adapter.addAll(nedanDateView)
+        adapter!!.addAll(nedanDateView!!)
 
 
 
