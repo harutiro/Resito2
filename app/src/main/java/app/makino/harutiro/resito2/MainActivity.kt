1607df74f8d96f970f3d169b94767664d64eb368
harutiro <harutiro2027@outlook.jp>
2021-01-10 13:15:27 +0900

    make: Initial commit

---
+++ app/src/main/java/app/makino/harutiro/resito2/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.makino.harutiro.resito2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
