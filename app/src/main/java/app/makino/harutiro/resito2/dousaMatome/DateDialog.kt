8c9cf35874429d1e9d0a7eea1a13521e2c02436f
harutiro <harutiro2027@outlook.jp>
2021-02-07 19:00:20 +0900

    refactor: ダイアログを固有のクラス化

---
+++ app/src/main/java/app/makino/harutiro/resito2/dousaMatome/DateDialog.kt
@@ -0,0 +1,49 @@
+package app.makino.harutiro.resito2.dousaMatome
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import app.makino.harutiro.resito2.input.henkan
+import java.util.*
+
+class DateDialog(val motoDate: String ,private val onSelected: (String) -> Unit) :
+            DialogFragment(), DatePickerDialog.OnDateSetListener {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val c = Calendar.getInstance()
+
+
+            //デフォルト設定の設定部分
+            val year = henkan(motoDate).yyyy().toInt()
+            val month = henkan(motoDate).mm().toInt()
+            val date = henkan(motoDate).dd().toInt()
+
+
+            //デフォルト設定出力部分
+            return DatePickerDialog(requireActivity(), this, year, month, date)
+        }
+
+        //帰ってきたデータを送り返すところ
+        override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+            //データを書き換えるよう
+            val month1 = month + 1
+
+            //基本データ書き込み
+            var monthString = month1.toString()
+            var dayOfMonthString = dayOfMonth.toString()
+
+            //データが一桁ならゼロ追加
+            if(month1 < 10){
+                monthString = "0" + month1.toShort()
+            }
+            if(dayOfMonth < 10){
+                dayOfMonthString = "0" + dayOfMonth.toString()
+            }
+
+
+            onSelected("${year}年 ${monthString}月 ${dayOfMonthString}日")
+
+        }
+    }
--- app/src/main/java/app/makino/harutiro/resito2/input/TestInput.kt
+++ app/src/main/java/app/makino/harutiro/resito2/input/TestInput.kt
@@ -30,6 +30,7 @@ import androidx.core.net.toUri
 import androidx.fragment.app.DialogFragment
 import app.makino.harutiro.resito2.OkaneListDateSaveRealm
 import app.makino.harutiro.resito2.R
+import app.makino.harutiro.resito2.dousaMatome.DateDialog
 import io.realm.Realm
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -373,49 +374,6 @@ class TestInput : AppCompatActivity() {
                 }
 
             }
-        }
-    }
-
-
-
-    //======================================ダイアログの出力部分======================================
-    class DateDialog(val motoDate: String ,private val onSelected: (String) -> Unit) :
-            DialogFragment(), DatePickerDialog.OnDateSetListener {
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            val c = Calendar.getInstance()
-
-
-            //デフォルト設定の設定部分
-            val year = henkan(motoDate).yyyy().toInt()
-            val month = henkan(motoDate).mm().toInt()
-            val date = henkan(motoDate).dd().toInt()
-
-
-            //デフォルト設定出力部分
-            return DatePickerDialog(requireActivity(), this, year, month, date)
-        }
-
-        //帰ってきたデータを送り返すところ
-        override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-
-            //データを書き換えるよう
-            val month1 = month + 1
-
-            //基本データ書き込み
-            var monthString = month1.toString()
-            var dayOfMonthString = dayOfMonth.toString()
-
-            //データが一桁ならゼロ追加
-            if(month1 < 10){
-                monthString = "0" + month1.toShort()
-            }
-            if(dayOfMonth < 10){
-                dayOfMonthString = "0" + dayOfMonth.toString()
-            }
-
-
-            onSelected("${year}年 ${monthString}月 ${dayOfMonthString}日")
-
         }
     }
 
