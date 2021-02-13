54733da7ca35167b152f938f87c26618c2852af6
harutiro <harutiro2027@outlook.jp>
2021-02-13 11:11:19 +0900

    add: SaihuRealmDate型完成

---
+++ app/src/main/java/app/makino/harutiro/resito2/setting/saihu/SaihuSettingListDateSaveRealm.kt
@@ -0,0 +1,10 @@
+package app.makino.harutiro.resito2.setting.saihu
+
+import io.realm.RealmObject
+
+open class SaihuSettingListDateSaveRealm(
+
+    open var name:String = "",
+    open var image:String =""
+
+):RealmObject()
